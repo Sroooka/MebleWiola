ffff257a1c68e37046b815fced50427b5f7f0f67
Sroka <tomasz.sroka@capgemini.com>
2019-02-12 18:10:55 +0100

    offer done + home image based on shop opening hours

--- src/app/ui/home/home.component.ts
+++ src/app/ui/home/home.component.ts
@@ -7,11 +7,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  srcShop = null;
+  day: Date = new Date();
+  weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  weekday = null;
+  hours = null;
+  minutes = null;
   constructor() {
 
    }
 
   ngOnInit() {
+    this.updateTime();
+    this.updateImageSource();
   }
 
+  updateTime(){
+    this.day = new Date();
+    this.weekday = this.weekdays[this.day.getDay()];
+    this.hours = this.day.getHours();
+    this.minutes = this.day.getMinutes();
+    console.log("Time: " + this.hours + ":" + this.minutes + " Day of week: " + this.weekday);
+  }
+
+  updateImageSource(){
+    if(this.checkIfShopIsOpened()){
+      this.srcShop = './assets/Media/Shop/shop_day.png';
+    } else{
+      this.srcShop = './assets/Media/Shop/shop_night.png';
+    }
+  }
+
+  checkIfShopIsOpened(){
+    if(this.weekday == "Monday" ||
+        this.weekday == "Tuesday" ||
+        this.weekday == "Wednesday" ||
+        this.weekday == "Thursday" ||
+        this.weekday == "Friday"){
+          // Normal day
+          // from 9-18
+          var start = 9;
+          var stop = 18;
+          if(this.hours >= start && this.hours < stop){
+            //open
+            return true;
+          } else {
+            //closed
+            return false;
+          }
+        } else if(this.weekday == "Saturday"){
+          // Saturday
+          // from 9-13:30
+          var start = 9;
+          var stop = 13;
+          if(this.hours >= start && this.hours < stop){
+            //open
+            return true;
+          } else if(this.hours == stop){
+            if(this.minutes < 30){
+              //open
+              return true;
+            } else{
+              //closed
+              return false;
+            }
+          } else{
+            //closed
+            return false;
+          }
+        } else if(this.weekday == "Sunday"){
+          // Sunday
+          // from 9-13:00
+          var start = 9;
+          var stop = 13;
+          if(this.hours >= start && this.hours < stop){
+            //open
+            return true;
+          } else{
+            //closed
+            return false;
+          }
+        }
+    return true;
+  }
 }
--- src/app/ui/offer/offer.component.ts
+++ src/app/ui/offer/offer.component.ts
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferComponent implements OnInit {
 
-  constructor() { }
+  srcDrewmax = './assets/Media/Offer/drewmaxlogo.png';
+  srcGala = './assets/Media/Offer/galalogo.png';
+  srcHalmar = './assets/Media/Offer/halmarlogo.png';
+  srcKliber = './assets/Media/Offer/kliberlogo.png';
+  srcKolo = './assets/Media/Offer/kolologo.png';
+  srcMerwina = './assets/Media/Offer/merwinalogo.png';
+
+  constructor( ) { }
 
   ngOnInit() {
   }
 
+  highlight(image:any, name:string) :void{
+    switch(name){
+      case "drewmax":
+        this.srcDrewmax='./assets/Media/Offer/drewmaxphoto.png';
+        break;
+      case "gala":
+        this.srcGala='./assets/Media/Offer/galaphoto.png';
+        break;
+      case "halmar":
+        this.srcHalmar='./assets/Media/Offer/halmarphoto.png';
+        break;
+      case "kliber":
+        this.srcKliber='./assets/Media/Offer/kliberphoto.png';
+        break;
+      case "kolo":
+        this.srcKolo='./assets/Media/Offer/kolophoto.png';
+        break;
+      case "merwina":
+        this.srcMerwina='./assets/Media/Offer/merwinaphoto.png';
+        break;
+      default:
+        break;
+    }
+  }
+
+  unhighlight(image:any, name:string) : void{
+    switch(name){
+      case "drewmax":
+        this.srcDrewmax='./assets/Media/Offer/drewmaxlogo.png';
+        break;
+      case "gala":
+        this.srcGala='./assets/Media/Offer/galalogo.png';
+        break;
+      case "halmar":
+        this.srcHalmar='./assets/Media/Offer/halmarlogo.png';
+        break;
+      case "kliber":
+        this.srcKliber='./assets/Media/Offer/kliberlogo.png';
+        break;
+      case "kolo":
+        this.srcKolo='./assets/Media/Offer/kolologo.png';
+        break;
+      case "merwina":
+        this.srcMerwina='./assets/Media/Offer/merwinalogo.png';
+        break;
+      default:
+        break;
+    }
+  }
 }
